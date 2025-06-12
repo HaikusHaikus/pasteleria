c68884ac03055141cefd6f0dcdd748dfacd5903a
HaikusHaikus <argalicia08@gmail.com>
2025-06-12 09:32:50 -0600

    server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 
 //conexion a mongoDB
 //concetarse a pasteleria
-mongoose.connect(process.env.MONGODB_URL, {   
+mongoose.connect(process.env.MONGODB_URI, {   
     useNewUrlParser:true, //usa el parser de url
     useUnifiedTopology:true //motor de monitoreo
    })
